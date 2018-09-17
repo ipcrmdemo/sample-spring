2220a1e28164d55763b95111d91e6983949b137c
ipcrm <mcadorette@gmail.com>
2018-09-16 22:36:53 -0400

    Initial commit from Atomist

---
+++ src/main/java/com/me/spring/QuickSpringApplication.java
@@ -0,0 +1,12 @@
+package com.me.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickSpringApplication.class, args);
+	}
+}
